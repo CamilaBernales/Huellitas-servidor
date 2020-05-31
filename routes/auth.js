ea82f57addce06ce515ae7e2c4b97dee53ddb957
Camila Bernales <camilabernales09@gmail.com>
2020-05-31 20:30:07 -0300

    cambios en login

--- routes/auth.js
+++ routes/auth.js
@@ -6,10 +6,7 @@ const { check } = require("express-validator");
 router.post(
   "/login",
   [
-    check("email", "Ingrese un email valido").isEmail(),
-    check("password", "El password debe ser minimo de 6 caracteres").isLength({
-      min: 6,
-    }),
+    check("email", "Ingrese un email valido").isEmail()
   ],
   authController.autenticarUsuario
 );
